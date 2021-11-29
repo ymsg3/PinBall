e268265a2e595207ad85aebcbebe8489669e6c3b
ymsg3 <95019206+ymsg3@users.noreply.github.com>
2021-11-29 22:37:11 +0900

    課題後（発展課題取り組み前）
    Lesson5の課題に取り組んだ時点の状態を取っておくためにコミット。

---
+++ Assets/PointRegulator.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PointRegulator : MonoBehaviour
+{
+
+    // 得点を表示するテキスト
+    GameObject pointText;
+
+    // 小さい星の点数
+    int smallStarPoint = 100;
+    // 大きい星の点数
+    int largeStarPoint = 250;
+    // 小さい雲の点数
+    int smallCloudPoint = 500;
+    // 大きい雲の点数
+    int largeCloudPoint = 1000;
+
+    void Start ()
+    {
+        // シーン中のPointTextオブジェクトを取得
+        this.pointText = GameObject.Find ("PointText");
+    }
+
+    void Update ()
+    {
+
+    }
+
+    void OnCollisionEnter (Collision other)
+    {
+        // 衝突前の得点を取得
+        Text beforePointText = this.pointText.GetComponent<Text> ();
+
+        // 計算用に変換
+        int point = int.Parse (beforePointText.text);
+
+        // ターゲットの種類に応じて追加する得点を選択
+        if (other.transform.tag == "SmallStarTag")
+        {
+            point += smallStarPoint;
+        }
+        else if (other.transform.tag == "LargeStarTag")
+        {
+            point += largeStarPoint;
+        }
+        else if (other.transform.tag == "SmallCloudTag")
+        {
+            point += smallCloudPoint;
+        }
+        else if (other.transform.tag == "LargeCloudTag")
+        {
+            point += largeCloudPoint;
+        }
+
+        // 衝突後の得点を表示
+        beforePointText.text = point.ToString ();
+    }
+
+}
